03904e582dd5796f1aadd0af9cd7c2d58d81bd9d
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-20 01:28:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+let val = confirm('Do you want to proceed');
+if(val){
+	alert('You clicked OK. Proceeding...');
+} else {
+	alert('You clicked Cancel. Exiting...')
+}
